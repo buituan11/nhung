eaf202eb6cb0f2ec94aae87413369e6746c1ee6f
buituan11 <buituan71099@gmail.com>
2020-04-20 06:26:45 +0700

    Updates

---
+++ precache-manifest.fd369b06e6c0c856d456823020c56bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a8396926618acd423072f13a1e83f1",
+    "url": "/hbpdthaiha/index.html"
+  },
+  {
+    "revision": "5fa9e5b793a19bcc560c",
+    "url": "/hbpdthaiha/static/css/main.d0cea7d5.chunk.css"
+  },
+  {
+    "revision": "71a16e21f62b7d67ea5f",
+    "url": "/hbpdthaiha/static/js/2.90e94cbd.chunk.js"
+  },
+  {
+    "revision": "5fa9e5b793a19bcc560c",
+    "url": "/hbpdthaiha/static/js/main.487d68d8.chunk.js"
+  },
+  {
+    "revision": "a6fcb8fd830109619c6a",
+    "url": "/hbpdthaiha/static/js/runtime~main.998ff66a.js"
+  }
+]);
